6b69cf2a5408e2ea85f79d40724ea4e9c7e29b6b
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-17 22:10:26 +0200

    add unique pointers with custom deleters examples into main script

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,14 @@
 
 #include "unique_pointer.hpp"
 
+void customDeleter(int* pointer) {
+    delete pointer;
+    std::cout << "other pointer deleted !" << std::endl;
+}
+
 int main() {
+
+    /* simple unique pointers */
 
     constexpr int FIRST_VALUE {10};
     auto firstUniquePointer = createUniquePointer(FIRST_VALUE);
@@ -11,6 +18,19 @@ int main() {
     constexpr int SECOND_VALUE {0};
     auto secondUniquePointer = createUniquePointer(SECOND_VALUE);
     receiveUniquePointer(std::move(secondUniquePointer));
+
+    /* unique pointers with custom deleters */
+
+    auto deleter = [](int* pointer) {
+        delete pointer;
+        std::cout << "pointer deleted !" << std::endl;
+    };
+
+    std::unique_ptr<int, decltype(deleter)> pointer(new int(10), deleter);
+    std::cout << *pointer << std::endl;
+
+    std::unique_ptr<int, void (*)(int*)> otherPointer(new int(100), customDeleter);
+    std::cout << *otherPointer << std::endl;
 
     return EXIT_SUCCESS;
 }
